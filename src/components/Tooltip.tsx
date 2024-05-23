48e60f37e10ca93862c1a420534123ad61fbf497
Jillian Somera <jsomera@gmail.com>
2024-05-23 17:28:59 +1000

    Add artist data and playcount to tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,13 +1,19 @@
-const Tooltip = (data) => {
-  const { points: { itemData: { dataIndex }} } = data;
-  const track = data.playlistTracks[0][dataIndex].track;
+const Tooltip = (formattedData) => {
+  const { points: { itemData: { dataIndex }, series: { data }} } = formattedData;
+  const track = formattedData.playlistTracks[0][dataIndex].track;
   const trackPreviewUrl = track.preview_url;
   const trackAlbumImage = track.album.images[0].url;
+  const trackName = track.name;
+  const artistName = track.artists.map((artist) => artist.name).join(", ");
+  const playcount = new Intl.NumberFormat().format(data[dataIndex]);
 
   return (
-    <div>
+    <div className="tooltip">
       <audio autoPlay src={trackPreviewUrl} />
-      <img src={trackAlbumImage} />
+      <img src={trackAlbumImage} className="img" />
+      <h2>{trackName}</h2>
+      <h3>{artistName}</h3>
+      <h3>{playcount} plays</h3>
     </div>
   )
 }

6e3cceaa4bc288f5972812eea6bf99b7f970a2e5
Jillian Somera <jsomera@gmail.com>
2024-06-05 11:27:15 +1000

    Show award emoji on playcount up end

--- src/components/PlaycountCounter.tsx
+++ src/components/PlaycountCounter.tsx
@@ -1,16 +1,20 @@
+import { useState } from 'react';
 import CountUp from 'react-countup';
 
 const PlaycountCounter = ({ playcount }) => {
+  const [showAwardEmoji, setShowAwardEmoji] = useState(false);
+
   return (
     <CountUp
       start={10000000}
       end={playcount}
       delay={0}
-      onEnd={() => console.log('Ended! 👏')}
+      onEnd={() => setShowAwardEmoji(true)}
     >
       {({ countUpRef }) => (
         <div className=''>
-          <span ref={countUpRef} />
+          {showAwardEmoji && <span className='award'>🏆</span>}
+          <span className='count-up' ref={countUpRef} />
           <span> plays</span>
         </div>
       )}

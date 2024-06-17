ce0a4553e7661f0524b0ea27780952acc3df2e1d
Jillian Somera <jsomera@gmail.com>
2024-06-17 17:31:58 +1000

    WIP fixing types

--- src/components/CustomTooltip.tsx
+++ src/components/CustomTooltip.tsx
@@ -1,13 +1,19 @@
 import { TooltipProps } from "../utils/Types";
+import { Track } from "spotify-types";
 
 const CustomTooltip = (formattedData: TooltipProps) => {
   const { points: { itemData: { dataIndex }, series: { data }} } = formattedData;
   let trackPreviewUrl = "";
+  let track;
 
   if (dataIndex) {
-    const track = formattedData.playlistTracks?[0][dataIndex]?.track;
-    trackPreviewUrl = track?.preview_url;
-    const playcount = data[dataIndex];
+    const playlistTrack = formattedData.playlistTracks[0][dataIndex]
+    const playcount = data[dataIndex] ?? 0;
+
+    if (playlistTrack) {
+      track = playlistTrack.track as Track;
+      trackPreviewUrl = track?.preview_url
+    }
 
     formattedData.setCurrentTrack(track)
     formattedData.setCurrentPlaycount(playcount)

e64e6fea81ed43e9c7fd4cb901bfd6a6b365b4c8
Jillian Somera <jsomera@gmail.com>
2024-06-24 16:54:52 +1000

    Add tooltip text for playcount

--- src/components/CustomTooltip.tsx
+++ src/components/CustomTooltip.tsx
@@ -1,13 +1,18 @@
+import { valueFormatter } from "../utils/BarChartStyles";
 import { TooltipProps } from "../utils/Types";
+valueFormatter
 
 const CustomTooltip = (props: TooltipProps) => {
   const { currentTrack } = props;
   const trackPreviewUrl = currentTrack?.preview_url ?? '';
+  const playcount = currentTrack?.playcount ?? 0;
 
   return (
     <div className="tooltip">
       <audio autoPlay src={trackPreviewUrl} />
+      <p>{new Intl.NumberFormat().format(playcount)} plays</p>
     </div>
   )
 }
+
 export default CustomTooltip;

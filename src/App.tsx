b9f7cf14f0e2125e24afeb7e5b7b55e927f278b8
Jillian Somera <jsomera@gmail.com>
2024-07-05 11:34:23 +1000

    Add visitors count badge and tweak css

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ import {
   albumPlayCountBaseUrl,
   wikiUrl,
   spotifyPlaylistUrl,
+  dummyFormattedTrackData,
 } from '../data';
 
 import ArtistInfo from './components/ArtistInfo';
@@ -197,7 +198,7 @@ function App() {
         })
       }
 
-      setFormattedTracks(data);
+      setFormattedTracks(dummyFormattedTrackData);
 
       console.log(formattedTracks)
 
@@ -368,13 +369,14 @@ function App() {
               }}
             />
           </ResponsiveChartContainer>
+           <p className="chart-footnote">* based on <span><a href={spotifyPlaylistUrl}>this</a></span> Spotify playlist</p>
         </div>
       </div>
-      <hr />
+      <hr className="footer-hr" />
       <footer className="footer">
-        <p className="chart-footnote">* based on <span><a href={spotifyPlaylistUrl}>this</a></span> Spotify playlist</p>
         <div className="footer-links">
           <span>© 2024 built by @jaerodyne</span>
+          <a href="https://visitorbadge.io/status?path=https%3A%2F%2Fdrizzyvskdot.netlify.app"><img src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fdrizzyvskdot.netlify.app&countColor=%23263759" /></a>
           <button type="button">
           <a href="https://github.com/jaerodyne/drakevskendrick-spotifystats">
             <svg viewBox="0 0 16 16" width="16" height="16" className="octicon octicon-mark-github" aria-hidden="true"><path fill="white" d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path></svg>
